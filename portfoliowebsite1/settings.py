e93922fd9d4d34331d19d69b5c32a6d776c6ba38
danis <danisterim1@gmail.comm>
2022-02-10 13:11:49 +0300

    ready9

--- portfoliowebsite1/settings.py
+++ portfoliowebsite1/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+
 STATIC_URL = 'static/'
 STATIC_ROOT = 'staticfiles'
 STATICFILES_DIRS = [
